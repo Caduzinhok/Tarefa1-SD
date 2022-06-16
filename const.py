c84cae5fe1cc1925f053b98330c9f75aa145709b
Caduzinhok <carlos_fire1@outlook.com>
2022-06-16 14:35:38 -0300

    sec commit

---
+++ const.py
@@ -0,0 +1,5 @@
+CHAT_SERVER_HOST = "10.128.0.3"
+CHAT_SERVER_PORT = 5001
+
+# IDs de usuários registrados com os respectivos endereços IP e números de porta
+registry = {"Alice":("54.208.168.115",5002), "Bob":("44.202.37.187",5002)}
